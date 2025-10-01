705b76fba7ec511538fa04d5aac7a645588dac08
Insu Hong <hinsu93@gmail.com>
2025-10-01 19:11:14 +0900

    cron.js added

--- src/server.js
+++ src/server.js
@@ -5,9 +5,12 @@ import { ENV } from "./config/env.js";
 
 import { db } from "./config/db.js";
 import { favoritesTable } from "./db/schema.js";
+import job from "./config/cron.js";
 
 const app = express();
 const PORT = ENV.PORT;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.use(express.json());
 
